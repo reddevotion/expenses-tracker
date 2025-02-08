a9179557adef56f32fe462b92e243cf6fa96a06b
Valery Reddevotion <linkolnsid228@gmail.com>
2025-02-08 07:39:44 +0300

    first commit

--- src/components/expenses-form.tsx
+++ src/components/expenses-form.tsx
@@ -9,6 +9,13 @@ function ExpensesForm() {
     const {expenseToEdit, setExpenseToEdit} = useExpenseToEditContext()
 
     
+    async function handleAction(formData: FormData) {
+      if (expenseToEdit) {
+        await handleUpdate(formData, expenseToEdit.id);
+      } else {
+        await createExpense(formData);
+      }
+    }
 
     async function handleUpdate(formData: FormData, id: number) {
         await updateExpense(formData, id)
@@ -17,9 +24,9 @@ function ExpensesForm() {
 
 
   return (
-    <motion.form action={(formData) => {expenseToEdit ? handleUpdate(formData, expenseToEdit?.id) : createExpense(formData)}} className='w-full mt-8 rounded overflow-hidden' initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{delay: 0.1}}>
+    <motion.form action={handleAction} className='w-full mt-8 rounded overflow-hidden' initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{delay: 0.1}}>
         <input type="text" name='description' placeholder='Description' className='w-full px-3 py-2 outline-none' defaultValue={expenseToEdit?.description || ''}  />
-        <input type="number" name='amount' placeholder='Amount' className='w-full px-3 py-2 outline-none' defaultValue={expenseToEdit?.amount || 0}/>
+        <input type="number" name='amount' placeholder='Amount' className='w-full px-3 py-2 outline-none' defaultValue={expenseToEdit?.amount}/>
         <button className='w-full bg-blue-500 text-white p-2 font-bold hover:bg-blue-600 transition '>{expenseToEdit ? 'Update Expense' : 'Add Expense'}</button>
     </motion.form>
   )
